b8b4134e573b227a6c1218a41410499b479b284d
qigoh <qigoh@s.sfusd.edu>
2026-03-16 03:01:30 -0700

    Small laser gun fix

--- mechanical-revolution/scripts/laser.gd
+++ mechanical-revolution/scripts/laser.gd
@@ -2,6 +2,7 @@ class_name Laser extends RayCast2D
 
 var damage := 5.0
 var direction := Vector2.ZERO
+var range := 1000.0
 
 func _init(look: Vector2, dps: float) -> void:
 	direction = look
@@ -12,7 +13,7 @@ func _ready() -> void:
 	pass
 
 func _physics_process(delta: float) -> void:
-	position += direction * delta
+	target_position = direction * range
 	
 	if is_colliding():
 		if (get_collider() is Enemy):
