d677dba0726b25a9447f989aaef94ec7d696d839
ProbablyJaylan <jaxie1@s.sfusd.edu>
2026-03-18 11:39:17 -0700

    IM SO FRIED

--- mechanical-revolution/scripts/weapontest.gd
+++ mechanical-revolution/scripts/weapontest.gd
@@ -1,5 +1,6 @@
 extends Weapon
 ## This timer's wait_time serves as the cooldown for the weapon
+@onready var pistol_sprite: Sprite2D = %PistolSprite
 
 func fire(lookVector:Vector2) -> void:
 	if _can_fire:
@@ -10,6 +11,7 @@ func fire(lookVector:Vector2) -> void:
 		)
 	else:
 		return
+	pistol_sprite.rotation = get_angle_to(lookVector.orthogonal())
 	var newProjectile : PlayerProjectileGB= attack.instantiate()
 	newProjectile.direction = lookVector
 	get_tree().current_scene.add_child(newProjectile)
