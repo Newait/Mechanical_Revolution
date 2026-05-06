eeeb1fc456ccc879bb00f61d5c057174eebf7bec
qigoh <qigoh@s.sfusd.edu>
2026-05-06 01:42:20 -0700

    Changes for demo

--- mechanical-revolution/scripts/invisible_teleport_block.gd
+++ mechanical-revolution/scripts/invisible_teleport_block.gd
@@ -1,14 +1,15 @@
-extends Node2D
+extends Area2D
 
+@export var tp_scene :PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	body_entered.connect(on_body_entered)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+#func _process(delta: float) -> void:
+	#pass
 
-func on_body_entered():
-	load("res://scenes/LevelFolder/Elevator.tscn")
+func on_body_entered(body:Node2D):
+	get_tree().change_scene_to_packed(tp_scene)
