ef2b9c35bc261f87d4e310d22248cac69a834362
qigoh <qigoh@s.sfusd.edu>
2026-05-13 14:13:01 -0700

    Grapple

--- mechanical-revolution/scripts/grapple.gd
+++ mechanical-revolution/scripts/grapple.gd
@@ -3,12 +3,18 @@ class_name Grapple extends RayCast2D
 
 var is_tethering:=false
 var tether_position : Vector2
+var tether_length:= 0.0
 var is_slack := false
 var grapple_offset:= 0.0:
 	set(val):
 		if (val > 0.0) and is_tethering:
 			if (not is_slack):
-				
+				is_slack = true
+				var peak_counts := floorf(tether_length/20.0)
+				grapple_line.points = []
+				grapple_line.add_point(Vector2.ZERO)
+				for i in range(peak_counts):
+					grapple_line.add_point(position + (tether_position - position) * (i/peak_counts))
 
 func attach_tether() -> void:
 	tether_position = grapple_ray.get_collision_point()
